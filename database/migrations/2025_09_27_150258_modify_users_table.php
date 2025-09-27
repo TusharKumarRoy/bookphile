a02014ac6886f5286f5fed1d133c1d4a31436308
Tushar Kumar Roy <tusharkumarroy.dev@gmail.com>
2025-09-27 21:40:05 +0600

    update up() and down() in modify_users_table.php

--- database/migrations/2025_09_27_150258_modify_users_table.php
+++ database/migrations/2025_09_27_150258_modify_users_table.php
@@ -12,7 +12,11 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            //
+            $table->string('first_name')->after('name');
+            $table->string('last_name')->after('first_name');
+            $table->text('bio')->nullable()->after('email_verified_at');
+            $table->string('profile_image')->nullable()->after('bio');
+            $table->enum('role', ['user', 'admin', 'master_admin'])->default('user')->after('profile_image');
         });
     }
 
@@ -22,7 +26,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            //
+            $table->dropColumn(['first_name', 'last_name', 'bio', 'profile_image', 'role']);
         });
     }
 };
